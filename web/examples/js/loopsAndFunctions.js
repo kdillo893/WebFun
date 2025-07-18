a5a7f98221dc2cb04d7ce55f4ab3b2b1090dd5a9
Kevin Dillon <kdillo893@gmail.com>
2025-07-18 13:20:12 -0500

    more js examples, examples for quiz and html

---
+++ web/examples/js/loopsAndFunctions.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing loops and functions
+ */
+
+//loops
+
+//note: ++ will add 1 and set the new value to the variable
+let counter = 0;
+counter++;
+console.log(counter);
+//this can be before or after the variable,
+// they do different things for advanced programmers
+++counter;
+
+//you can also do -- for subtracting 1
+counter--;
+--counter;
+
+console.log(counter);
+
+for (let i = 0; i < 10; ++i) {
+
+}
