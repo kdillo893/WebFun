6296a49c2eda31464fbda77b190f3023f82f1dbe
Kevin Dillon <kdillo893@gmail.com>
2025-07-24 15:48:19 -0500

    javascript events, loops, functions, css

--- web/js/keyboardListener.js
+++ web/js/keyboardListener.js
@@ -1,8 +1,15 @@
 const keyBufferSize = 2048;
-let keyLogBuffer = Array<key;
+let keyLogBuffer = [];
 let keyLogIdx = 0;
 
-function keyLogger(event) {
+
+
+/**
+ * TODO: add a function to process the keyboard event and place it in a buffer
+ *  which is flushed to an output log when full or at a given interval
+ * @param {Event} event - the event info from a key press (keydown, keyup, etc.)
+ */
+function keyLogger(event, fileout) {
     const key = event.key;
 
     if (event.key) {
@@ -13,19 +20,134 @@ function keyLogger(event) {
 
         }
     }
-
-
-
     
 }
 
-document.addEventListener("keydown", (event) => {
+function changeText(event) {
+    if (event == null) {
+        return;
+    }
+
     const key = event.key;
 
     if (key) {
-        console.log(key);
+        //step1
+        const outputElement = document.getElementById("output-element");
+
+        //step2
+        const newLine = document.createElement("span");
+        const textNode = document.createTextNode(key);
+        newLine.appendChild(textNode);
+
+        //step 3
+        outputElement.appendChild(newLine);
     }
-    console.log(event.code);
-    console.log("ctrlKey pressed:" + event.ctrlKey);
-    console.log(typeof(event.key));
-});
+}
+
+document.addEventListener("keydown", changeText);
+
+
+
+function clearOutput() {
+    const outputElement = document.getElementById("output-element");
+    outputElement.innerHTML = null;
+}
+
+function addNumbers() {
+    const leftInput = document.querySelector("input[name='leftNumber'");
+    const rightInput = document.querySelector("input[name='rightNumber'");
+
+    if (leftInput.value == null || rightInput.value == null) {
+        return;
+    }
+
+    let sum = parseInt(leftInput.value) + parseInt(rightInput.value);
+
+    const resultsEl = document.getElementById("calculator-results");
+
+    const newLine = document.createElement("p");
+    const textNode = document.createTextNode(sum);
+    newLine.appendChild(textNode);
+
+    resultsEl.appendChild(newLine);
+}
+
+function calculate(operation) {
+    
+    console.log(operation);
+    if (operation == null) {
+        return;
+    }
+
+    const leftInput = document.querySelector("input[name='leftNumber']");
+    const rightInput = document.querySelector("input[name='rightNumber']");
+
+    if (!leftInput.value || !rightInput.value) {
+        return;
+    }
+
+    let result;
+    const leftNum = parseInt(leftInput.value);
+    const rightNum = parseInt(rightInput.value);
+
+    switch(operation) {
+        case '+':
+            result = leftNum + rightNum;
+            break;
+        case '-':
+            result = leftNum - rightNum;
+    }
+    let sum = parseInt(leftInput.value) + parseInt(rightInput.value);
+
+    const resultsEl = document.getElementById("calculator-results");
+
+    const newLine = document.createElement("p");
+    const textNode = document.createTextNode(sum);
+    newLine.appendChild(textNode);
+
+    resultsEl.appendChild(newLine);
+}
+
+function clearCalculator() {
+    const outputElement = document.getElementById("calculator-results");
+    outputElement.innerHTML = null;
+}
+
+
+
+
+
+
+
+
+function showClickEvent(event) {
+    
+}
+
+
+        //Don't type this yet;
+        // const outputElement = document.getElementById("key-output");
+        // const outLine = document.createElement("p");
+        // const 
+        // outputElement.appendChild()
+    
+
+
+
+
+//check key presses with keyboard input.
+// document.addEventListener("keydown", (event) => {
+//     const key = event.key;
+
+//     if (key) {
+//         console.log(key);
+//     }
+//     console.log(event.code);
+//     console.log("ctrlKey pressed:" + event.ctrlKey);
+//     console.log(typeof(event.key));
+
+//     const keyOutElement = document.getElementById("key-output");
+//     if (keyOutElement && key) {
+//         keyOutElement.innerText = key;
+//     }
+// });
--- web/js/loopsAndFunctions.js
+++ web/js/loopsAndFunctions.js
@@ -19,6 +19,84 @@ counter--;
 console.log(counter);
 
 console.log("This prints numbers 0 through 9")
+
 for (let i = 0; i < 10; ++i) {
     console.log(i);
 }
+
+//declare array
+var array1 = [1,12, 23, 52];
+
+//set value in the array.
+array1[0] = 100;
+array1[3] = 1990;
+
+array1.push(919191919)
+
+console.log("using for-each on array.")
+
+function displayValue(value) {
+    console.log(value);
+}
+array1.forEach(displayValue);
+
+// array1.pop()
+
+console.log("Array values:")
+for (let i = 0; i < array1.length ; i++) {
+    console.log(array1[i]);
+    console.log(`index=${i}, value=${array1[i]}`);
+}
+
+var someWords = "I am making a variable with words!";
+var eachWord = someWords.split(" ");
+
+for (let i = 0; i < someWords.length; i++) {
+    console.log(someWords[i]);
+}
+
+
+console.log(eachWord);
+
+
+
+
+//challenge:
+//  Make a paragraph in a variable, 
+//  count the number of words starting with "t":
+//HINT1: use "split" for splitting words
+//Hint2: checking the first letter of a word is done with this:
+
+var myParagraph = "This is the web development class. My name is Mr. D. I am "
+    + "typing a bunch of things in this variable so that I can use it "
+    + "as an example. I am also using the + key so I can make spaces "
+    + "without going to the right."
+
+var commaSeparatedThings = "hi,this,is,an,example,with,commas";
+var commaWords = commaSeparatedThings.split(",");
+
+var someWords = "I am making a variable with words!";
+var eachWord = someWords.split(" ");
+
+var wordsArray = myParagraph.split(" ");
+
+//check if a word starts with the letter "t" lowercase.
+// var startsWithT = word.startsWith("t");
+// console.log(startsWithT);
+
+//variable counting words starting with t
+let wordsStartingWithT = 0;
+
+//loop going over each word in your paragraph.
+for (let i = 0; i < wordsArray.length; i++) {
+
+    let word = wordsArray[i];
+    if (word != null && word.toLowerCase().startsWith("t")) {
+        wordsStartingWithT = wordsStartingWithT + 1;
+    }
+}
+
+console.log("Words starting with t: " + wordsStartingWithT);
+
+// var word = "the";
+// word = word.toLowerCase();
